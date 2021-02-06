bb6746c0ece85d5d16d775173ea141a3b692a6f0
Aedan Lawrence <aedan@mrinterbugs.uk>
2021-02-06 21:56:50 +0000

    Auto stash before merge of "login-system" and "origin/main"

--- lib/screens/TeacherHome.dart
+++ lib/screens/TeacherHome.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/components/hamburger.dart';
+import 'package:flutter_app/components/Hamburger.dart';
 
 class TeacherHome extends StatelessWidget {
   TeacherHome({
