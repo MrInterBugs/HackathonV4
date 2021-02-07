232e4d99a60bdb1d7b80e9ba5b65ba77139f9a29
chazzox <42206116+chazzox@users.noreply.github.com>
2021-02-07 02:04:24 +0000

    improved hamburger menu styles

--- lib/components/hamburger.dart
+++ lib/components/hamburger.dart
@@ -9,73 +9,79 @@ class Hamburger extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 150,
-      // Add a ListView to the drawer. This ensures the user can scroll
-      // through the options in the drawer if there isn't enough vertical
-      // space to fit everything.
-      child: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Menu', style:TextStyle(color: Color(0xfffe8019),fontSize:35)),
-              decoration: BoxDecoration(color: Color(0xff282828)),
-            ),
-            // Home
-            ListTile(
-              title: Text('Home Page'),
-              onTap: () {
-                Navigator.pushNamed(context, '/');
-              },
-            ),
-            // Teachers View
-            ListTile(
-              title: Text('Teachers View'),
-              onTap: () {
-                Navigator.pushNamed(context, '/teacherView');
-              },
-            ),
-            ListTile(
-              title: Text('Student View'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-                // Then close the drawer.
-                Navigator.pushNamed(context, '/studentView');
-              },
-            ),
-            ListTile(
-              title: Text('Messages'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-                // Then close the drawer.
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Timetable'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-                // Then close the drawer.
-                Navigator.pop(context);
-              },
-            ),
-            // Logout Button
-            ListTile(
-              title: Text('Logout'),
-              onTap: () {
-                signOutGoogle();
-                SystemNavigator.pop();
-              },
-            ),
+    return Theme(
+        data: Theme.of(context).copyWith(canvasColor: Color(0xff2d2b2a)),
+        child: Container(
+          width: 150,
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: Drawer(
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Text('Menu',
+                      style: TextStyle(color: Color(0xfffe8019), fontSize: 35)),
+                ),
+                // Home
+                ListTile(
+                  title: Text('Home Page'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                ),
+                // Teachers View
 
-          ],
-        ),
-      ),
-    );
+                CustomListTile(
+                    title: 'Teachers View', routeName: '/teacherView'),
+                CustomListTile(
+                    title: 'Student View', routeName: '/studentView'),
+                ListTile(
+                  title: Text('Messages'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Timetable'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                // Logout Button
+                ListTile(
+                  title: Text('Logout'),
+                  onTap: () {
+                    signOutGoogle();
+                    SystemNavigator.pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  final String title;
+  final String routeName;
+
+  final Function stateMutations;
+
+  CustomListTile(
+      {@required this.title, @required this.routeName, this.stateMutations});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        title: Text(this.title),
+        selected: ModalRoute.of(context).settings.name == this.routeName,
+        onTap: () {
+          if (this.stateMutations != null) this.stateMutations();
+          Navigator.pushNamed(context, this.routeName);
+        });
   }
 }
